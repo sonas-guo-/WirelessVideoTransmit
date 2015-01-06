709d77705523c1b74af02d790d327f69cc707e3b
Guo linsen <guolinsen123@163.com>
2015-01-06 23:05:37 +0800

    加入chart
    加入chart，实现图表框架

--- WirelessTransmit/WirelessTransmit/Form1.Designer.cs
+++ WirelessTransmit/WirelessTransmit/Form1.Designer.cs
@@ -28,62 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.TcpPage = new System.Windows.Forms.TabPage();
-            this.SerialPortPage = new System.Windows.Forms.TabPage();
-            this.IPText = new System.Windows.Forms.TextBox();
-            this.OpenConnection = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.CloseConnection = new System.Windows.Forms.Button();
-            this.tabControl1.SuspendLayout();
+            this.OpenConnection = new System.Windows.Forms.Button();
+            this.IPText = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.TcpPage.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.TcpPage);
-            this.tabControl1.Controls.Add(this.SerialPortPage);
-            this.tabControl1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(845, 588);
-            this.tabControl1.TabIndex = 0;
             // 
             // TcpPage
             // 
             this.TcpPage.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.TcpPage.Controls.Add(this.chart1);
             this.TcpPage.Controls.Add(this.CloseConnection);
-            this.TcpPage.Controls.Add(this.pictureBox1);
             this.TcpPage.Controls.Add(this.OpenConnection);
             this.TcpPage.Controls.Add(this.IPText);
             this.TcpPage.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TcpPage.Location = new System.Drawing.Point(4, 29);
             this.TcpPage.Name = "TcpPage";
             this.TcpPage.Padding = new System.Windows.Forms.Padding(3);
-            this.TcpPage.Size = new System.Drawing.Size(837, 555);
+            this.TcpPage.Size = new System.Drawing.Size(1062, 555);
             this.TcpPage.TabIndex = 0;
             this.TcpPage.Text = "Tcp";
             // 
-            // SerialPortPage
+            // CloseConnection
             // 
-            this.SerialPortPage.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.SerialPortPage.Location = new System.Drawing.Point(4, 29);
-            this.SerialPortPage.Name = "SerialPortPage";
-            this.SerialPortPage.Padding = new System.Windows.Forms.Padding(3);
-            this.SerialPortPage.Size = new System.Drawing.Size(837, 555);
-            this.SerialPortPage.TabIndex = 1;
-            this.SerialPortPage.Text = "SerialPort";
-            this.SerialPortPage.UseVisualStyleBackColor = true;
-            // 
-            // IPText
-            // 
-            this.IPText.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.IPText.Location = new System.Drawing.Point(6, 75);
-            this.IPText.Name = "IPText";
-            this.IPText.Size = new System.Drawing.Size(173, 27);
-            this.IPText.TabIndex = 0;
+            this.CloseConnection.Location = new System.Drawing.Point(97, 127);
+            this.CloseConnection.Name = "CloseConnection";
+            this.CloseConnection.Size = new System.Drawing.Size(82, 38);
+            this.CloseConnection.TabIndex = 4;
+            this.CloseConnection.Text = "关闭";
+            this.CloseConnection.UseVisualStyleBackColor = true;
+            this.CloseConnection.Click += new System.EventHandler(this.CloseConnection_Click);
             // 
             // OpenConnection
             // 
@@ -96,50 +80,88 @@
             this.OpenConnection.UseVisualStyleBackColor = true;
             this.OpenConnection.Click += new System.EventHandler(this.OpenConnection_Click);
             // 
-            // pictureBox1
+            // IPText
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.AliceBlue;
-            this.pictureBox1.Location = new System.Drawing.Point(212, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(619, 543);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.IPText.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.IPText.Location = new System.Drawing.Point(6, 75);
+            this.IPText.Name = "IPText";
+            this.IPText.Size = new System.Drawing.Size(173, 27);
+            this.IPText.TabIndex = 0;
             // 
-            // CloseConnection
+            // tabControl1
             // 
-            this.CloseConnection.Location = new System.Drawing.Point(97, 127);
-            this.CloseConnection.Name = "CloseConnection";
-            this.CloseConnection.Size = new System.Drawing.Size(82, 38);
-            this.CloseConnection.TabIndex = 4;
-            this.CloseConnection.Text = "关闭";
-            this.CloseConnection.UseVisualStyleBackColor = true;
-            this.CloseConnection.Click += new System.EventHandler(this.CloseConnection_Click);
+            this.tabControl1.Controls.Add(this.TcpPage);
+            this.tabControl1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1070, 588);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea4.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea4.AxisX.Interval = 1D;
+            chartArea4.AxisX.LineWidth = 2;
+            chartArea4.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea4.AxisX.Maximum = 12D;
+            chartArea4.AxisX.Minimum = 0D;
+            chartArea4.AxisX.Title = "time/s";
+            chartArea4.AxisY.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea4.AxisY.Interval = 5D;
+            chartArea4.AxisY.LineWidth = 2;
+            chartArea4.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea4.AxisY.Maximum = 45D;
+            chartArea4.AxisY.Minimum = -45D;
+            chartArea4.AxisY.Title = "temperature/℃";
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(217, 3);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "Temperature";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(842, 549);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(869, 612);
+            this.ClientSize = new System.Drawing.Size(1109, 667);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
             this.TcpPage.ResumeLayout(false);
             this.TcpPage.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage TcpPage;
-        private System.Windows.Forms.TabPage SerialPortPage;
-        private System.Windows.Forms.TextBox IPText;
-        private System.Windows.Forms.Button OpenConnection;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button CloseConnection;
+        private System.Windows.Forms.Button OpenConnection;
+        private System.Windows.Forms.TextBox IPText;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+
     }
 }
 
--- WirelessTransmit/WirelessTransmit/Form1.cs
+++ WirelessTransmit/WirelessTransmit/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
 
 namespace WirelessTransmit
 {
@@ -17,9 +20,25 @@ namespace WirelessTransmit
         public Form1()
         {
             InitializeComponent();
+            InitChart();
         }
 
+        private void InitChart()
+        {
+            //chart1.ChartAreas.
+            //List<double>[] listX=new List<double>[1];
+            //List<double>[] listY=new List<double>[1];
+            //chart1.Series[0].Points.DataBindXY(listX, listY);
+            //chart1.Series[0].Points.DataBindY(listY);
 
+
+            //chart1.ChartAreas["mainChart"].Points;
+            for (int i = 0; i < 10; i++)
+            {
+                chart1.Series["Temperature"].Points.AddXY(i, i*i);
+            }
+            //chart1.Show();
+        }
 
         private void OpenConnection_Click(object sender, EventArgs e)
         {
@@ -33,7 +52,7 @@ namespace WirelessTransmit
                 IPAddress localAddr = IPAddress.Parse(source[0]);
                 server = new TcpListener(localAddr, port);
 
-
+                
 
 
                 IPText.Enabled = false;
@@ -47,9 +66,21 @@ namespace WirelessTransmit
 
         private void CloseConnection_Click(object sender, EventArgs e)
         {
+
+
+
+
             IPText.Enabled = true;
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+
 
     }
+
+
 }
