8a2c94b8280628560e87b7e86c6cf28405ff4b57
Guo linsen <guolinsen123@163.com>
2015-01-11 23:11:31 +0800

    加入timer控件
    使用timer控件来进行动态显示折线图，尚未完成

--- WirelessTransmit/WirelessTransmit/Form1.Designer.cs
+++ WirelessTransmit/WirelessTransmit/Form1.Designer.cs
@@ -29,125 +29,190 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.TcpPage = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.portText = new System.Windows.Forms.TextBox();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.ipText = new System.Windows.Forms.TextBox();
+            this.ipLabel = new System.Windows.Forms.Label();
+            this.msgBox = new System.Windows.Forms.TextBox();
             this.CloseConnection = new System.Windows.Forms.Button();
             this.OpenConnection = new System.Windows.Forms.Button();
-            this.IPText = new System.Windows.Forms.TextBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chartUpdateTimer = new System.Windows.Forms.Timer(this.components);
             this.TcpPage.SuspendLayout();
-            this.tabControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TcpPage
             // 
+            this.TcpPage.AllowDrop = true;
             this.TcpPage.BackColor = System.Drawing.Color.LightSteelBlue;
             this.TcpPage.Controls.Add(this.chart1);
+            this.TcpPage.Controls.Add(this.portText);
+            this.TcpPage.Controls.Add(this.portLabel);
+            this.TcpPage.Controls.Add(this.ipText);
+            this.TcpPage.Controls.Add(this.ipLabel);
+            this.TcpPage.Controls.Add(this.msgBox);
             this.TcpPage.Controls.Add(this.CloseConnection);
             this.TcpPage.Controls.Add(this.OpenConnection);
-            this.TcpPage.Controls.Add(this.IPText);
             this.TcpPage.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TcpPage.Location = new System.Drawing.Point(4, 29);
+            this.TcpPage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TcpPage.Name = "TcpPage";
-            this.TcpPage.Padding = new System.Windows.Forms.Padding(3);
-            this.TcpPage.Size = new System.Drawing.Size(1062, 555);
+            this.TcpPage.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TcpPage.Size = new System.Drawing.Size(1181, 631);
             this.TcpPage.TabIndex = 0;
             this.TcpPage.Text = "Tcp";
             // 
+            // chart1
+            // 
+            this.chart1.AllowDrop = true;
+            chartArea2.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Lines;
+            chartArea2.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisX.Interval = 1D;
+            chartArea2.AxisX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisX.LineWidth = 2;
+            chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea2.AxisX.Maximum = 12D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisX.ScaleBreakStyle.Enabled = true;
+            chartArea2.AxisX.Title = "time/s";
+            chartArea2.AxisY.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Lines;
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea2.AxisY.Interval = 5D;
+            chartArea2.AxisY.LineWidth = 2;
+            chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea2.AxisY.Maximum = 45D;
+            chartArea2.AxisY.Minimum = -45D;
+            chartArea2.AxisY.Title = "temperature/℃";
+            chartArea2.CursorY.IsUserEnabled = true;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(371, 4);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chart1.Name = "chart1";
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Color = System.Drawing.Color.Red;
+            series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series3.Legend = "Legend1";
+            series3.Name = "Temperature";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Humidity";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(807, 623);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart1";
+            this.chart1.TextAntiAliasingQuality = System.Windows.Forms.DataVisualization.Charting.TextAntiAliasingQuality.Normal;
+            // 
+            // portText
+            // 
+            this.portText.Location = new System.Drawing.Point(105, 160);
+            this.portText.Name = "portText";
+            this.portText.ReadOnly = true;
+            this.portText.Size = new System.Drawing.Size(97, 27);
+            this.portText.TabIndex = 11;
+            // 
+            // portLabel
+            // 
+            this.portLabel.Location = new System.Drawing.Point(11, 157);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(92, 30);
+            this.portLabel.TabIndex = 10;
+            this.portLabel.Text = "端口:";
+            this.portLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // ipText
+            // 
+            this.ipText.Location = new System.Drawing.Point(106, 116);
+            this.ipText.Name = "ipText";
+            this.ipText.ReadOnly = true;
+            this.ipText.Size = new System.Drawing.Size(96, 27);
+            this.ipText.TabIndex = 9;
+            // 
+            // ipLabel
+            // 
+            this.ipLabel.Location = new System.Drawing.Point(7, 112);
+            this.ipLabel.Name = "ipLabel";
+            this.ipLabel.Size = new System.Drawing.Size(96, 34);
+            this.ipLabel.TabIndex = 8;
+            this.ipLabel.Text = "本地IP:";
+            this.ipLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // msgBox
+            // 
+            this.msgBox.Location = new System.Drawing.Point(8, 209);
+            this.msgBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.msgBox.Multiline = true;
+            this.msgBox.Name = "msgBox";
+            this.msgBox.Size = new System.Drawing.Size(194, 416);
+            this.msgBox.TabIndex = 6;
+            // 
             // CloseConnection
             // 
-            this.CloseConnection.Location = new System.Drawing.Point(97, 127);
+            this.CloseConnection.Location = new System.Drawing.Point(106, 33);
+            this.CloseConnection.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CloseConnection.Name = "CloseConnection";
-            this.CloseConnection.Size = new System.Drawing.Size(82, 38);
+            this.CloseConnection.Size = new System.Drawing.Size(96, 51);
             this.CloseConnection.TabIndex = 4;
-            this.CloseConnection.Text = "关闭";
+            this.CloseConnection.Text = "停止服务";
             this.CloseConnection.UseVisualStyleBackColor = true;
             this.CloseConnection.Click += new System.EventHandler(this.CloseConnection_Click);
             // 
             // OpenConnection
             // 
             this.OpenConnection.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.OpenConnection.Location = new System.Drawing.Point(6, 127);
+            this.OpenConnection.Location = new System.Drawing.Point(7, 33);
+            this.OpenConnection.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.OpenConnection.Name = "OpenConnection";
-            this.OpenConnection.Size = new System.Drawing.Size(85, 38);
+            this.OpenConnection.Size = new System.Drawing.Size(96, 51);
             this.OpenConnection.TabIndex = 1;
-            this.OpenConnection.Text = "打开";
+            this.OpenConnection.Text = "启动服务";
             this.OpenConnection.UseVisualStyleBackColor = true;
             this.OpenConnection.Click += new System.EventHandler(this.OpenConnection_Click);
-            // 
-            // IPText
-            // 
-            this.IPText.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.IPText.Location = new System.Drawing.Point(6, 75);
-            this.IPText.Name = "IPText";
-            this.IPText.Size = new System.Drawing.Size(173, 27);
-            this.IPText.TabIndex = 0;
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.TcpPage);
             this.tabControl1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(14, 16);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1070, 588);
+            this.tabControl1.Size = new System.Drawing.Size(1189, 664);
             this.tabControl1.TabIndex = 0;
             // 
-            // chart1
+            // chartUpdateTimer
             // 
-            chartArea4.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
-            chartArea4.AxisX.Interval = 1D;
-            chartArea4.AxisX.LineWidth = 2;
-            chartArea4.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
-            chartArea4.AxisX.Maximum = 12D;
-            chartArea4.AxisX.Minimum = 0D;
-            chartArea4.AxisX.Title = "time/s";
-            chartArea4.AxisY.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
-            chartArea4.AxisY.Interval = 5D;
-            chartArea4.AxisY.LineWidth = 2;
-            chartArea4.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
-            chartArea4.AxisY.Maximum = 45D;
-            chartArea4.AxisY.Minimum = -45D;
-            chartArea4.AxisY.Title = "temperature/℃";
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
-            this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
-            legend4.Name = "Legend1";
-            this.chart1.Legends.Add(legend4);
-            this.chart1.Location = new System.Drawing.Point(217, 3);
-            this.chart1.Name = "chart1";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Color = System.Drawing.Color.Red;
-            series4.Legend = "Legend1";
-            series4.Name = "Temperature";
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(842, 549);
-            this.chart1.TabIndex = 5;
-            this.chart1.Text = "chart1";
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.chartUpdateTimer.Tick += new System.EventHandler(this.chartUpdateTimer_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1109, 667);
+            this.ClientSize = new System.Drawing.Size(1237, 783);
             this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.TcpPage.ResumeLayout(false);
             this.TcpPage.PerformLayout();
-            this.tabControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -157,10 +222,14 @@
         private System.Windows.Forms.TabPage TcpPage;
         private System.Windows.Forms.Button CloseConnection;
         private System.Windows.Forms.Button OpenConnection;
-        private System.Windows.Forms.TextBox IPText;
         private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TextBox msgBox;
+        public System.Windows.Forms.Label ipLabel;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.TextBox ipText;
+        private System.Windows.Forms.TextBox portText;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer chartUpdateTimer;
 
     }
 }
--- WirelessTransmit/WirelessTransmit/Form1.cs
+++ WirelessTransmit/WirelessTransmit/Form1.cs
@@ -10,73 +10,194 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using System.Windows.Forms.DataVisualization.Charting;
-
+using System.Threading;
 
 
 namespace WirelessTransmit
 {
     public partial class Form1 : Form
     {
+        Thread threadWatch = null;
+        Socket socketWatch = null;
+        Socket socketConnection = null;
+
+        IPAddress clientIP; //访问客户端的IP
+        int clientPort; //访问客户端的端口号
+
+        public const int SendBufferSize = 2 * 1024;
+        public const int ReceiveBufferSize = 8 * 1024;
+
+        public float temperature;
+        public float humidity;
+        Queue<double> temperatureQueue = new Queue<double>();
+        Queue<double> humidityQueue = new Queue<double>();
+
         public Form1()
         {
             InitializeComponent();
             InitChart();
+            chartDisplay();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
         }
 
         private void InitChart()
         {
-            //chart1.ChartAreas.
-            //List<double>[] listX=new List<double>[1];
-            //List<double>[] listY=new List<double>[1];
-            //chart1.Series[0].Points.DataBindXY(listX, listY);
-            //chart1.Series[0].Points.DataBindY(listY);
-
-
-            //chart1.ChartAreas["mainChart"].Points;
-            for (int i = 0; i < 10; i++)
+            int i;
+            for (i = 0; i <= chart1.ChartAreas[0].AxisX.Maximum;i++ )
             {
-                chart1.Series["Temperature"].Points.AddXY(i, i*i);
+                temperatureQueue.Enqueue(0);
+                humidityQueue.Enqueue(0);
             }
-            //chart1.Show();
+
+
+
+           //chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.True;
+           //chart1.ChartAreas[0].AxisY.Enabled = AxisEnabled.True;
+        }
+        
+        private void receiveData()
+        {
+            while (true)
+            {
+                try
+                {
+                    socketConnection = socketWatch.Accept();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message); //提示套接字监听异常
+                    break;
+                }
+                clientIP = (socketConnection.RemoteEndPoint as IPEndPoint).Address;//获取访问客户端的IP
+                clientPort = (socketConnection.RemoteEndPoint as IPEndPoint).Port;//获取访问客户端的Port
+                ParameterizedThreadStart pts = new ParameterizedThreadStart(ServerRecMsg);//创建通信线程 
+                Thread thread = new Thread(pts);
+                thread.IsBackground = true;
+                thread.Start(socketConnection);//启动线程
+                msgBox.AppendText("IP: " + clientIP + " Port: " + clientPort + " 的客户端与您连接成功,现在你们可以开始通信了...\r\n");
+            }
+        }
+        /// 获取本地IPv4地址
+        private IPAddress GetLocalIPv4Address()
+        {
+            IPAddress localIPv4 = null;
+            IPAddress[] ipAddressList = Dns.GetHostAddresses(Dns.GetHostName());//获取本机所有的IP地址列表
+            foreach (IPAddress ipAddress in ipAddressList)
+            {
+                //判断是否是IPv4地址
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork) //AddressFamily.InterNetwork表示IPv4 
+                {
+                    localIPv4 = ipAddress;
+                }
+                else
+                    continue;
+            }
+            return localIPv4;
         }
 
         private void OpenConnection_Click(object sender, EventArgs e)
         {
-            TcpListener server = null;
-            Int32 port ;
-            string[] source=new string[2];
-            try
-            {
-                source = IPText.Text.Split(':');
-                port = Convert.ToInt32(source[1]);
-                IPAddress localAddr = IPAddress.Parse(source[0]);
-                server = new TcpListener(localAddr, port);
-
-                
-
-
-                IPText.Enabled = false;
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("请输入IP地址");
-            }
-
+            //定义一个套接字用于监听客户端发来的信息  包含3个参数(IP4寻址协议,流式连接,TCP协议)
+            socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //发送信息 需要1个IP地址和端口号
+            //获取服务端IPv4地址
+            IPAddress ipAddress = GetLocalIPv4Address();
+            ipText.Text=ipAddress.ToString(); 
+            int port = 8000;//开启的本地端口号
+            portText.Text = port.ToString();
+            IPEndPoint endpoint = new IPEndPoint(ipAddress, port);//将IP地址和端口号绑定到网络节点endpoint上 
+            socketWatch.Bind(endpoint);//将负责监听的套接字绑定网络端点
+            socketWatch.Listen(20);//将套接字的监听队列长度设置为20
+            threadWatch = new Thread(receiveData);//创建一个负责监听客户端的线程 
+            threadWatch.IsBackground = true;//将窗体线程设置为与后台同步
+            threadWatch.Start();//启动线程
+            msgBox.AppendText("服务器已经启动,开始监听客户端传来的信息!" + "\r\n");
+            OpenConnection.Enabled = false;
+            CloseConnection.Enabled = true;
         }
 
         private void CloseConnection_Click(object sender, EventArgs e)
         {
-
-
-
-
-            IPText.Enabled = true;
+            socketWatch.Close();
+            //socketWatch = null;
+            threadWatch.DisableComObjectEagerCleanup();
+            OpenConnection.Enabled = true;
+            CloseConnection.Enabled = false;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        /// 接收客户端发来的信息
+        private void ServerRecMsg(object socketClientPara)
+        {
+            Socket socketServer = socketClientPara as Socket;
+            bool temperatureStart=false;
+            
+            while (true)
+            {
+                /*int dataCount = 0;
+                byte[] buffer = new byte[ReceiveBufferSize];
+                try
+                {
+                    //获取接收的数据,并存入内存缓冲区  返回一个字节数组的长度
+                    if (socketServer != null) dataCount = socketServer.Receive(buffer);
+                    if (dataCount > 0) 
+                    {
+                        string sourceStr;
+                        sourceStr=System.Text.Encoding.Default.GetString(buffer);
+                        //dataSolution(sourceStr);
+                        //msgBox.AppendText(System.Text.Encoding.Default.GetString(buffer));
+                        //msgBox.AppendText("\n");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    break;
+                }
+                */
+                
+
+
+
+
+            }
+        }
+        public void dataSolution(string source)
         {
 
+
+
+
         }
+        public void chartDisplay()
+        {
+            int i;
+            for (i=0;i<temperatureQueue.Count;i++)
+            {
+                chart1.Series[0].Points.AddXY(i,temperatureQueue.ElementAt(i));
+                chart1.Series[1].Points.AddXY(i,humidityQueue.ElementAt(i));
+
+            }
+
+
+
+        }
+
+        private void chartUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            temperatureQueue.Enqueue(temperature);
+            humidityQueue.Enqueue(humidity);
+
+            temperatureQueue.Dequeue();
+            humidityQueue.Dequeue();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            chartDisplay();
+
+        }
+
+
 
 
 
